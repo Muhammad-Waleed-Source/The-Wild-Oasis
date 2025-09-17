72a04fe33d0fdb3618b80d1690c3d247601b84db
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-09-17 23:04:40 +0500

    Form

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,8 +4,21 @@ export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
   }
 
   return data;
@@ -15,7 +28,6 @@ export async function deleteCabin(id) {
   const { error } = await supabase.from("cabins").delete().eq("id", id);
 
   if (error) {
-    console.error(error);
     throw new Error("Cabin could not be deleted");
   }
 }
