b6d4f71a32a4bed40cf25324e6ed5d67e73be360
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-11-04 22:56:21 +0500

    Bookings Table

--- src/data/Uploader.jsx
+++ src/data/Uploader.jsx
@@ -140,11 +140,11 @@ function Uploader() {
     >
       <h3>SAMPLE DATA</h3>
 
-      <Button onClick={uploadAll} disabled={isLoading}>
+      <Button onClick={uploadAll} disabled={isLoading} size="medium" variation="primary">
         Upload ALL
       </Button>
 
-      <Button onClick={uploadBookings} disabled={isLoading}>
+      <Button onClick={uploadBookings} disabled={isLoading} size="medium" variation="primary">
         Upload bookings ONLY
       </Button>
     </div>
